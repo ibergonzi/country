191ae20ecdb71a41d2c9fff09085e9ca5335ee3b
Ivan Bergonzi <ibergonzi@gmail.com>
2017-11-15 18:40:07 -0300

    Exportacion Agenda

--- frontend/views/agenda/index.php
+++ frontend/views/agenda/index.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use kartik\grid\GridView;
 
+use kartik\popover\PopoverX;
+
 /* @var $this yii\web\View */
 /* @var $searchModel frontend\models\AgendaSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -55,7 +57,22 @@ $this->registerCss('
     <h3><?= Html::encode($this->title) ?></h3>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 	<?php
-	
+	$lbl2='';
+	$pdfHeader=[
+				'L'=>['content'=>\Yii::$app->params['lblName']],
+				'C'=>['content'=>$this->title . $lbl2,
+					  //'font-size' => 80,
+					  'font-style'=>'B'
+					  //'color'=> '#333333'
+					],
+				'R'=>['content'=>''],
+			
+		];
+	$pdfFooter=[
+		'L'=>['content'=>\Yii::$app->params['lblName2']],
+		'C'=>['content'=>'página {PAGENO} de {nb}'],
+		'R'=>['content'=>'Fecha:{DATE d/m/Y}'],
+		]; 	
 	$columns=[
             //'numero',
             'nombre',
@@ -83,9 +100,69 @@ $this->registerCss('
             ],
         ];
 
+		if (\Yii::$app->user->can('exportarListaPersonas')) {        
+			// contiene la selección inicial de columnas, es decir, todas
+			// por ejemplo [0,1,2,3]
+			$poSel=[];
+			// contiene las descripciones de las columnas
+			// por ejemplo [0=>'Portón', 1=>'Usuario',2=>'Fecha',3=>'Texto']
+			$poItems=[];
+			$i=-1;
+			foreach ($columns as $c) {
+				$i++;
+				// si es un array busca la clave "attribute"
+				if (is_array($c)) {
+					foreach ($c as $key=>$value) {
+						if ($key=='attribute') {
+							$poSel[]=$i;
+							$poItems[$i]=$searchModel->getAttributeLabel($value);
+							break;
+						}
+					}
+				} else {
+					$poSel[]=$i;
+					$poItems[$i]=$searchModel->getAttributeLabel($c);
+				}
+			}
+
+			// tiene que estar fuera del Pjax
+			echo PopoverX::widget([
+				'options'=>['id'=>'popControl'],
+				'placement' => PopoverX::ALIGN_RIGHT,
+				'toggleButton' => ['label'=>'<i class="glyphicon glyphicon-list"></i> Cols.a exportar', 
+									'class'=>'btn btn-default pull-left'],
+				'header'=>'Elija las columnas a exportar',
+				'size'=>'lg',
+				//'content'=>Html::checkboxList('exportColumns', [0,1,2,3], [0=>'Portón', 1=>'Usuario',2=>'Fecha',3=>'Texto'],
+				'content'=>Html::checkboxList('exportColumns', $poSel, $poItems,
+					['class'=>'form-control','tag'=>false,//'separator'=>'<br/>'
+					])											
+			]);
+			// para que no se encime con el summary del gridview	
+			//echo '<div class="clearfix"></div>';
+				
+		}		
+
+		
+		$contentToolbar=\nterms\pagesize\PageSize::widget([
+			'defaultPageSize'=>\Yii::$app->params['infracConceptos.defaultPageSize'],
+			'sizes'=>\Yii::$app->params['infracciones.sizes'],
+			'label'=>'',
+			'options'=>[
+					'class'=>'btn btn-default',
+					'title'=>'Cantidad de elementos por página',
+				],
+			]);		
+		if (\Yii::$app->user->can('exportarListaPersonas')) {			
+			$toolbar=['{export} ',['content'=>$contentToolbar],];
+		} else {
+			$toolbar=[['content'=>$contentToolbar]];
+		}
+
     echo GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'filterSelector' => 'select[name="per-page"]', 
 		'pjax'=>true,
 		'pjaxSettings'=>['neverTimeout'=>true,],  
         // Para que muestre todo el gridview, solo aplicable a kartik, el de yii anda bien
@@ -96,9 +173,114 @@ $this->registerCss('
 		//'condensed'=>true, 
 		//'layout'=>'&nbsp;{toolbar}{summary}{items}{pager}',
 
+		'layout'=>'&nbsp;{toolbar}{summary}{items}{pager}',
+
 		'pager' => [
 			'firstPageLabel' => true,
 			'lastPageLabel' => true,
-		],			
+		],	
+		'export' => [
+			'label' => 'Exportar',
+			'fontAwesome' => true,
+		    'showConfirmAlert'=>true,	
+		    'target'=>GridView::TARGET_BLANK,			
+		],
+		
+		'toolbar' => $toolbar,
+	
+		
+		'exportConfig' => [
+			GridView::PDF => [
+					//'label' => Yii::t('kvgrid', 'PDF'),
+					//'icon' => $isFa ? 'file-pdf-o' : 'floppy-disk',
+					//'iconOptions' => ['class' => 'text-danger'],
+					//'showHeader' => true,
+					//'showPageSummary' => true,
+					//'showFooter' => true,
+					//'showCaption' => true,
+					//'filename' => Yii::t('kvgrid', 'grid-export'),
+					'filename' => $this->title,
+					//'alertMsg' => Yii::t('kvgrid', 'The PDF export file will be generated for download.'),
+					//'options' => ['title' => Yii::t('kvgrid', 'Portable Document Format')],
+					//'mime' => 'application/pdf',
+					'config' => [
+							//'mode' => 'c',
+							//'format' => 'A4-L',
+							'destination' => 'D',
+							//'destination' => 'I',
+							//'marginTop' => 20,
+							//'marginBottom' => 20,
+							/*
+							'cssInline' => '.kv-wrap{padding:20px;}' .
+								'.kv-align-center{text-align:center;}' .
+								'.kv-align-left{text-align:left;}' .
+								'.kv-align-right{text-align:right;}' .
+								'.kv-align-top{vertical-align:top!important;}' .
+								'.kv-align-bottom{vertical-align:bottom!important;}' .
+								'.kv-align-middle{vertical-align:middle!important;}' .
+								'.kv-page-summary{border-top:4px double #ddd;font-weight: bold;}' .
+								'.kv-table-footer{border-top:4px double #ddd;font-weight: bold;}' .
+								'.kv-table-caption{font-size:1.5em;padding:8px;border:1px solid #ddd;border-bottom:none;}',
+							*/
+							
+							'methods' => [
+								'SetHeader' => [
+									['odd' => $pdfHeader, 'even' => $pdfHeader]
+								],
+								'SetFooter' => [
+									['odd' => $pdfFooter, 'even' => $pdfFooter]
+								],
+							],
+							
+							'options' => [
+								'title' => $this->title,
+								//'subject' => Yii::t('kvgrid', 'PDF export generated by kartik-v/yii2-grid extension'),
+								//'keywords' => Yii::t('kvgrid', 'krajee, grid, export, yii2-grid, pdf')
+								'subject' => '',
+								'keywords' => '',							],
+							'contentBefore'=>'',
+							'contentAfter'=>''
+					]			
+			
+			],
+			GridView::EXCEL => [
+					//'label' => Yii::t('kvgrid', 'Excel'),
+					//'icon' => $isFa ? 'file-excel-o' : 'floppy-remove',
+					//'iconOptions' => ['class' => 'text-success'],
+					//'showHeader' => true,
+					//'showPageSummary' => true,
+					//'showFooter' => true,
+					//'showCaption' => true,
+					'filename' => $this->title,
+					//'alertMsg' => Yii::t('kvgrid', 'The EXCEL export file will be generated for download.'),
+					//'options' => ['title' => Yii::t('kvgrid', 'Microsoft Excel 95+')],
+					//'mime' => 'application/vnd.ms-excel',
+					'config' => [
+						'worksheet' => $this->title,
+						'cssFile' => ''
+							]
+			],
+			GridView::CSV => [
+			
+					//'label' => Yii::t('kvgrid', 'CSV'),
+					//'icon' => $isFa ? 'file-code-o' : 'floppy-open', 
+					//'iconOptions' => ['class' => 'text-primary'],
+					//'showHeader' => true,
+					//'showPageSummary' => true,
+					//'showFooter' => true,
+					//'showCaption' => true,
+					'filename' => $this->title,
+					//'alertMsg' => Yii::t('kvgrid', 'The CSV export file will be generated for download.'),
+					//'options' => ['title' => Yii::t('kvgrid', 'Comma Separated Values')],
+					//'mime' => 'application/csv',
+					
+					'config' => [
+						'colDelimiter' => ";",
+						'rowDelimiter' => "\r\n",
+					]
+								
+			
+			],		
+		],				
     ]); ?>
 </div>
